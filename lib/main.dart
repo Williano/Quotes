cdbe970dc53c79c2172cf9389bbcb106900516d4
Williano <paawilly17@gmail.com>
2020-02-18 06:41:32 -0600

    Add stateful widget structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp();
+void main() => runApp(MaterialApp(
+  home: QuoteList(),
+));
+
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
 
 
