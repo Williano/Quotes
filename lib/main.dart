457afe988a20b7f6f807e451a1a49bef1954bfb3
Williano <paawilly17@gmail.com>
2020-02-18 06:51:48 -0600

    Display list of data in view

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: <Widget>[
-
-        ],
+        children: quotes.map((quote) => Text(quote)).toList(),
       ),
     );
   }
