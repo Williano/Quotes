c6b2d6a6fa7d12d32d11e1307ff18bd92c20f97a
Williano <paawilly17@gmail.com>
2020-02-18 06:46:08 -0600

    Add list data

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,27 @@ class QuoteList extends StatefulWidget {
 }
 
 class _QuoteListState extends State<QuoteList> {
+
+  List<String> quotes = [
+    "Be yourself; everyone else is already taken",
+    "I have nothing to declare except my genius",
+    "The truth is rarely pure and never simple"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text("Awesome Quotes"),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: <Widget>[
+
+        ],
+      ),
     );
   }
 }
